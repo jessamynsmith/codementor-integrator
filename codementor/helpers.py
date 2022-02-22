250f84fd63f69325ec7d81d462b549a54b4f11e6
jessamynsmith <geekchick77@gmail.com>
2022-02-22 09:09:18 -0700

    Use correct setting to determine if using SSL

--- codementor/helpers.py
+++ codementor/helpers.py
@@ -4,6 +4,6 @@ from django.contrib.sites.models import Site
 
 def get_full_domain():
     scheme = 'https'
-    if not settings.SECURE_SSL_REDIRECT:
+    if not settings.SSL_ENABLED:
         scheme = 'http'
     return '%s://%s' % (scheme, Site.objects.get_current().domain)
